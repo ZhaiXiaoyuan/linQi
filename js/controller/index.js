05e07cf3ab06fd9856dc003d0731ef28d81740c5
ZhaiXiaoyuan <2549004118@qq.com>
2019-02-21 02:58:06 +0800

    20190221

--- js/controller/index.js
+++ js/controller/index.js
@@ -316,4 +316,35 @@ $(function () {
             }
         })
     }
+
+    /**
+     * 临时测试
+     */
+    function websocketTest(id) {
+        console.log('___________'+id+'____________');
+        // 初始化一个 WebSocket 对象
+        var ws = new WebSocket("ws://zyu-server.wicp.net:55788/123");
+
+// 建立 web socket 连接成功触发事件
+        ws.onopen = function (evt) {
+            // 使用 send() 方法发送数据
+            ws.send("发送数据");
+            console.log("数据发送中**",id,'**:',evt);
+        };
+
+// 接收服务端数据时触发事件
+        ws.onmessage = function (evt) {
+            var received_msg = evt.data;
+            console.log("数据已接收**",id,'**:'+received_msg);
+        };
+
+// 断开 web socket 连接成功触发事件
+        ws.onclose = function (evt) {
+            console.log("连接已关闭**",id,'**:',evt);
+        };
+    }
+/*
+    for(var i=0;i<3;i++){
+        websocketTest(i)
+    }*/
 })
